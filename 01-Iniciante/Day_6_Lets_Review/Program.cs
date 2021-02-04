9b453942f741c4414c71118810952981ceb98798
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-04 20:30:55 -0300

    commit inicial.

---
+++ 01-Iniciante/Day_6_Lets_Review/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_6_Lets_Review
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
