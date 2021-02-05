64ed1a206317b5488e06db7d33d7e3ba4d43efaf
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-04 21:01:41 -0300

    commit final

--- 01-Iniciante/Day_6_Lets_Review/Program.cs
+++ 01-Iniciante/Day_6_Lets_Review/Program.cs
@@ -6,7 +6,30 @@ namespace Day_6_Lets_Review
     {
         static void Main( string[] args )
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string txt = Console.ReadLine();
+                char[] vetChar = txt.ToCharArray();
+
+                for (int j = 0; j < vetChar.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        Console.Write(vetChar[j]);
+                    }
+                }
+                Console.Write(" ");
+                for (int j = 0; j < vetChar.Length; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        Console.Write(vetChar[j]);
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
