b74e69c81815dfc2d048fe6ec3fbac47eae356d4
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-05 11:45:34 -0300

    commit final

--- 01-Iniciante/Day_7_Arrays/Program.cs
+++ 01-Iniciante/Day_7_Arrays/Program.cs
@@ -6,7 +6,14 @@ namespace Day_7_Arrays
     {
         static void Main( string[] args )
         {
-            Console.WriteLine("Hello World!");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            Array.Reverse(arr);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
         }
     }
 }
