b296c00d940715f8da9b80d0e304a482d103aab9
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-07 13:44:06 -0300

    link youtube hakerRank. commit final

--- 01-Iniciante/Day_9_Recursion_3/Program.cs
+++ 01-Iniciante/Day_9_Recursion_3/Program.cs
@@ -1,16 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 namespace Day_9_Recursion_3
 {
@@ -18,6 +6,8 @@ namespace Day_9_Recursion_3
     {
         /*
          * https://pt.wikipedia.org/wiki/Recursividade_(ci%C3%AAncia_da_computa%C3%A7%C3%A3o)#:~:text=do%20computador%20moderno.-,Programa%C3%A7%C3%A3o%20recursiva,triviais%20e%20n%C3%A3o%20envolvem%20recurs%C3%A3o.&text=Aqui%20est%C3%A1%20a%20mesma%20fun%C3%A7%C3%A3o%20codificada%20sem%20recurs%C3%A3o.
+         * 
+         * https://youtu.be/glENxqtJzAQ
          */
 
         static int factorial( int n )
