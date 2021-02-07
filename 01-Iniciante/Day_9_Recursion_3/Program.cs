23f5ee488ae91dd14b1f4f2fe65657d8b00495d8
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-07 12:46:07 -0300

    commit final

--- 01-Iniciante/Day_9_Recursion_3/Program.cs
+++ 01-Iniciante/Day_9_Recursion_3/Program.cs
@@ -19,18 +19,18 @@ namespace Day_9_Recursion_3
         /*
          * https://pt.wikipedia.org/wiki/Recursividade_(ci%C3%AAncia_da_computa%C3%A7%C3%A3o)#:~:text=do%20computador%20moderno.-,Programa%C3%A7%C3%A3o%20recursiva,triviais%20e%20n%C3%A3o%20envolvem%20recurs%C3%A3o.&text=Aqui%20est%C3%A1%20a%20mesma%20fun%C3%A7%C3%A3o%20codificada%20sem%20recurs%C3%A3o.
          */
-        
+
         static int factorial( int n )
         {
-            if (n == 0 || n == 1)
+            if (n <= 1)
             {
                 return 1;
             }
             else
             {
-                return n* factorial(n - 1);
+                return n * factorial(n - 1);
             }
-            
+
         }
 
         static void Main( string[] args )
