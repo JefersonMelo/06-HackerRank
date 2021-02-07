f008b9177e9f1b1f60d1879a29c5316a9f3040d6
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-07 11:44:27 -0300

    commit final.

--- 01-Iniciante/Day_9_Recursion_3/Program.cs
+++ 01-Iniciante/Day_9_Recursion_3/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
 namespace Day_9_Recursion_3
 {
@@ -7,9 +19,33 @@ namespace Day_9_Recursion_3
         /*
          * https://pt.wikipedia.org/wiki/Recursividade_(ci%C3%AAncia_da_computa%C3%A7%C3%A3o)#:~:text=do%20computador%20moderno.-,Programa%C3%A7%C3%A3o%20recursiva,triviais%20e%20n%C3%A3o%20envolvem%20recurs%C3%A3o.&text=Aqui%20est%C3%A1%20a%20mesma%20fun%C3%A7%C3%A3o%20codificada%20sem%20recurs%C3%A3o.
          */
+        
+        static int factorial( int n )
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n* factorial(n - 1);
+            }
+            
+        }
+
         static void Main( string[] args )
         {
-            
+            // comentado pois gera erro ao utilizar o trecho na ide.
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+            //textWriter subistituído por console.
+            Console.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
         }
     }
 }
