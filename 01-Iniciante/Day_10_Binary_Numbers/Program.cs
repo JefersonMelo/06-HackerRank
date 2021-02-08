d718c2f7be719ac3db37a21084357b4d4c71f14f
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-08 20:18:17 -0300

    commit final

--- 01-Iniciante/Day_10_Binary_Numbers/Program.cs
+++ 01-Iniciante/Day_10_Binary_Numbers/Program.cs
@@ -6,7 +6,26 @@ namespace Day_10_Binary_Numbers
     {
         static void Main( string[] args )
         {
-            
+            int n, cont = 0, resp = 0;
+            n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    cont++;
+                    if (cont > resp)
+                    {
+                        resp = cont;
+                    }
+                }
+                else
+                {
+                    cont = 0;
+                }
+                n /= 2;
+            }
+            Console.WriteLine(resp);
         }
     }
 }
