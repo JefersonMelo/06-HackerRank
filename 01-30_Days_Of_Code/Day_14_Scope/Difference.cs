16a5d1fc8d300ee5c760dfbfe65fee9cf936a853
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-23 20:27:22 -0300

    commit final

---
+++ 01-30_Days_Of_Code/Day_14_Scope/Difference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day_14_Scope
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        // Add your code here
+        public Difference(int[] array)
+        {
+            this.elements = array;
+        }
+
+        public int computeDifference()
+        {
+            Array.Sort(elements);
+            maximumDifference = Math.Abs(elements[elements.Length - 1] - elements[0]);
+            return maximumDifference;
+        }
+
+    } // End of Difference Class
+}
--- 01-30_Days_Of_Code/Day_14_Scope/Program.cs
+++ 01-30_Days_Of_Code/Day_14_Scope/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day_14_Scope
 {
     class Program
     {
-        static void Main( string[] args )
+        /*
+        https://www.youtube.com/watch?reload=9&v=sba-AMLc9lw 
+        https://hackerrankweb.wordpress.com/2016/11/30/day-14-scope/
+        */
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
         }
     }
 }
