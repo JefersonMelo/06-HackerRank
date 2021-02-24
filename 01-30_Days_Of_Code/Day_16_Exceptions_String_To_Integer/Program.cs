b2a1297f6f97c5cd466d6f09385907c7082fedb6
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-23 21:13:31 -0300

    commit inicial

---
+++ 01-30_Days_Of_Code/Day_16_Exceptions_String_To_Integer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_16_Exceptions_String_To_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
