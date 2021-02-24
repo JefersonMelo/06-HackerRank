297f7ad563505ecfc9f0758094ec71f90992bcd9
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-24 12:37:01 -0300

    commit inicio problema

--- 01-30_Days_Of_Code/Day_20_Sorting/Program.cs
+++ 01-30_Days_Of_Code/Day_20_Sorting/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day_20_Sorting
 {
     class Program
     {
-        static void Main(string[] args)
+        /*
+         https://www.hackerrank.com/challenges/30-sorting/problem
+        https://www.hackerrank.com/challenges/30-sorting/tutorial
+        https://en.wikipedia.org/wiki/Sorting_algorithm
+
+         */
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            // Write Your Code Here
         }
     }
 }
