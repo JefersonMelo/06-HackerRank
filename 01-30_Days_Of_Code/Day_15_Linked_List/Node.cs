86375d05842b3bd6244949c216b11a3690e33ac1
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-23 21:13:20 -0300

    commit final

---
+++ 01-30_Days_Of_Code/Day_15_Linked_List/Node.cs
@@ -0,0 +1,34 @@
+﻿namespace Day_15_Linked_List
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node() { }
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+        public Node insert(Node head, int data)
+        {
+            //Complete this method
+            Node no = new Node(data);
+            Node temp = head;
+            if (head == null)
+            {
+                head = no;
+                return head;
+            }
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = no;
+            return head;
+        }
+    }
+}
--- 01-30_Days_Of_Code/Day_15_Linked_List/Program.cs
+++ 01-30_Days_Of_Code/Day_15_Linked_List/Program.cs
@@ -2,11 +2,33 @@
 
 namespace Day_15_Linked_List
 {
+    /*
+     https://hackerrankweb.wordpress.com/2016/11/30/day-15-linked-list/
+     */
     class Program
     {
-        static void Main(string[] args)
+        static Node no = new Node();
+
+        public static void display(Node head)
         {
-            Console.WriteLine("Hello World!");
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+        static void Main(String[] args)
+        {
+
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = no.insert(head, data);
+            }
+            display(head);
         }
     }
 }
