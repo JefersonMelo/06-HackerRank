070f65c75d20596356a8c5f6371e9a876eb30976
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-23 22:22:07 -0300

    commit final

---
+++ 01-30_Days_Of_Code/Day_17_More_Exceptions/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day_17_More_Exceptions
+{
+    class Calculator
+    {
+        //public Calculator() { }
+
+        public int power(int n, int p)
+        {
+            int a;
+
+            if (n < 0 || p < 0)
+            {
+                throw new Exception("n and p should be non-negative");
+            }
+            else
+            {
+                a = (int)Math.Pow(n, p);
+            }
+
+            return a;
+        }
+    }
+}
--- 01-30_Days_Of_Code/Day_17_More_Exceptions/Program.cs
+++ 01-30_Days_Of_Code/Day_17_More_Exceptions/Program.cs
@@ -4,9 +4,31 @@ namespace Day_17_More_Exceptions
 {
     class Program
     {
-        static void Main(string[] args)
+        /*
+        
+        https://docs.microsoft.com/pt-br/dotnet/csharp/language-reference/keywords/throw
+        https://www.hackerrank.com/challenges/30-more-exceptions/problem
+        */
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator myCalculator = new Calculator();
+
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+                try
+                {
+                    int ans = myCalculator.power(n, p);
+                    Console.WriteLine(ans);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
