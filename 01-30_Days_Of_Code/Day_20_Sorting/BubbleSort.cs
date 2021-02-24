9696c02007dbf86e8c2b3b3b78f15a25fa904c2d
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-24 13:51:57 -0300

    commit aceito. final

---
+++ 01-30_Days_Of_Code/Day_20_Sorting/BubbleSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_20_Sorting
+{
+    class BubbleSort
+    {
+        public void bubbleSort(int[] x, int n)
+        {
+            int posicaoFinal = x.Length - 1;
+            int trocarPosicao, aux;
+            int tocasNum = 0;
+
+            while (posicaoFinal > 0)
+            {
+                trocarPosicao = 0;
+
+                for (int i = 0; i < posicaoFinal; i++)
+                {
+                    if (x[i] > x[i + 1])
+                    {
+                        aux = x[i];
+                        x[i] = x[i + 1];
+                        x[i + 1] = aux;
+
+                        trocarPosicao = i;
+                        tocasNum++;
+                    }
+                }
+                posicaoFinal = trocarPosicao;
+            }
+            Console.WriteLine($"Array is sorted in {tocasNum} swaps.\n" +
+                              $"First Element: {x[0]}\n" +
+                              $"Last Element: {x[n - 1]}");
+        }
+    }
+}
--- 01-30_Days_Of_Code/Day_20_Sorting/Program.cs
+++ 01-30_Days_Of_Code/Day_20_Sorting/Program.cs
@@ -8,17 +8,21 @@ namespace Day_20_Sorting
     class Program
     {
         /*
-         https://www.hackerrank.com/challenges/30-sorting/problem
+        https://www.hackerrank.com/challenges/30-sorting/problem
         https://www.hackerrank.com/challenges/30-sorting/tutorial
         https://en.wikipedia.org/wiki/Sorting_algorithm
-
+        https://hackerrankweb.wordpress.com/2016/12/07/day-20-sorting/
          */
+        static BubbleSort bubble = new BubbleSort();
+
         static void Main(String[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
             string[] a_temp = Console.ReadLine().Split(' ');
             int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
             // Write Your Code Here
+            bubble.bubbleSort(a, n);
         }
     }
 }
