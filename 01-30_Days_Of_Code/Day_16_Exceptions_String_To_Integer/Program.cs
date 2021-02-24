8e325375e405877d16b798d4ad31b261b7fb3abe
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-23 21:37:14 -0300

    commit com ajuste e comentário.

--- 01-30_Days_Of_Code/Day_16_Exceptions_String_To_Integer/Program.cs
+++ 01-30_Days_Of_Code/Day_16_Exceptions_String_To_Integer/Program.cs
@@ -4,13 +4,16 @@ namespace Day_16_Exceptions_String_To_Integer
 {
     class Program
     {
+        /*
+        https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/tutorial
+        */
         static void Main(string[] args)
         {
             string s = Console.ReadLine();
 
             try
             {
-                int x = int.Parse(Console.ReadLine());
+                int x = int.Parse(s);
                 Console.WriteLine(x);
             }
             catch (Exception)
