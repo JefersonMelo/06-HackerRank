a986b91c31a8ea6a0758e38d2a0d4242d6afcf5f
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-24 10:12:43 -0300

    commit inicial

---
+++ 01-30_Days_Of_Code/Day_19_Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_19_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
