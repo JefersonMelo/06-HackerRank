30d3145419b1d8477978bdca0a6890aa7e69d585
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-25 20:45:14 -0300

    commit ok, mas não aceito no hakerrank

---
+++ 01-30_Days_Of_Code/21_Generics/Printer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_21_Generics
+{
+    public class Printer<T>
+    {
+        /**
+	*    Name: PrintArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+        // Write your code here
+        public void PrintArray(T t)
+        {
+            foreach (var item in t.ToString())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
--- 01-30_Days_Of_Code/21_Generics/Program.cs
+++ 01-30_Days_Of_Code/21_Generics/Program.cs
@@ -6,8 +6,10 @@ namespace Day_21_Generics
     {
 		/*
         https://www.hackerrank.com/challenges/30-generics/tutorial
-        https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/
+        https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/generics/
          */
+		static Printer<int> p = new Printer<int>();
+		static Printer<int> ps = new Printer<int>();
 		static void Main(string[] args)
 		{
 			int n = Convert.ToInt32(Console.ReadLine());
@@ -23,9 +25,10 @@ namespace Day_21_Generics
 			{
 				stringArray[i] = Console.ReadLine();
 			}
-
-			PrintArray<Int32>(intArray);
-			PrintArray<String>(stringArray);
+			p.PrintArray(intArray.Length);
+			ps.PrintArray(stringArray.Length);
+			//PrintArray<int>(intArray);
+			//PrintArray<string>(stringArray);
 		}
 	}
 }
