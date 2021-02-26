f0761bc451f04b1d2e2d2f971b72f2e4e82833c7
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-25 21:11:49 -0300

    commit da tarefa proposta

---
+++ 01-30_Days_Of_Code/Day_22_Binary_Search_Trees/Node.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_22_Binary_Search_Trees
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+
+        public Node() { }
+
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+
+        public Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        public int getHeight(Node root)
+        {
+            //Write your code here
+            if (root == null)
+            {
+                return -1;
+            }
+            else
+            {
+                int esquerda = getHeight(root.left);
+                int direita = getHeight(root.right);
+                return Math.Max(esquerda, direita) + 1;
+            }
+
+        }
+
+    }
+
+}
--- 01-30_Days_Of_Code/Day_22_Binary_Search_Trees/Program.cs
+++ 01-30_Days_Of_Code/Day_22_Binary_Search_Trees/Program.cs
@@ -4,9 +4,24 @@ namespace Day_22_Binary_Search_Trees
 {
     class Program
     {
-        static void Main(string[] args)
+        /*https://www.hackerrank.com/challenges/30-binary-search-trees/tutorial
+          https://hackerrankweb.wordpress.com/2016/12/07/day-22-binary-search-trees/
+         */
+
+        static Node node = new Node();
+
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = node.insert(root, data);
+            }
+            int height = node.getHeight(root);
+            Console.WriteLine(height);
+
         }
     }
 }
