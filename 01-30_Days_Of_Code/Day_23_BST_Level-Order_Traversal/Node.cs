d43430b9f3549022f8fb3b802261960129a5b32b
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-25 21:17:50 -0300

    commit com a atividade proposta

---
+++ 01-30_Days_Of_Code/Day_23_BST_Level-Order_Traversal/Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_23_BST_Level_Order_Traversal
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+
+        public Node() { }
+
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+        public void levelOrder(Node root)
+        {
+            //Write your code here
+        }
+
+        public Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+    }
+}
---
+++ 01-30_Days_Of_Code/Day_23_BST_Level-Order_Traversal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day_23_BST_Level_Order_Traversal
+{
+    class Program
+    {
+        /*
+         * https://www.hackerrank.com/challenges/30-binary-trees/tutorial
+         */
+        static Node node = new Node();
+
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = node.insert(root, data);
+            }
+            node.levelOrder(root);
+
+        }
+    }
+}
