66a1780a77b0b17838737140664eeef85bc1ad6f
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-01 14:38:06 -0300

    commit renomeio

---
+++ 01-30_Days_Of_Code/26_Nested_Logic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_26_Nested_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
