bcd5dd07fba10b0585cd88f47f87e0ff259796ad
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-01 14:36:17 -0300

    commit final

---
+++ 01-30_Days_Of_Code/Day_25_Running_Time_And_Complexity/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day_25_Running_Time_And_Complexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int x;
+            bool primo;
+            for (int i = 0; i <= n; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                primo = true;
+                for (int j = 2; j * j < x; j++)
+                {
+                    if (x % j == 0)
+                    {
+                        primo = false;
+                        break;
+                    }
+                }
+                if (primo && x != 1)
+                {
+                    Console.WriteLine("Prime");
+                }
+                else
+                {
+                    Console.WriteLine("Not prime");
+                }
+            }
+        }
+    }
+}
