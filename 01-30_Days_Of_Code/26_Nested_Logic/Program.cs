da575fbb4b3d3989e1f5cd484fed7212a4bddb6a
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-03 17:16:25 -0300

    commit final

--- 01-30_Days_Of_Code/26_Nested_Logic/Program.cs
+++ 01-30_Days_Of_Code/26_Nested_Logic/Program.cs
@@ -6,7 +6,37 @@ namespace Day_26_Nested_Logic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] dataDevolucao = Console.ReadLine().Split(' ');
+            string[] dataRetirada = Console.ReadLine().Split(' ');
+
+            int multa = 0;
+
+            int diaRetirada = int.Parse(dataRetirada[0]);
+            int mesRetirada = int.Parse(dataRetirada[1]);
+            int anoRetirada = int.Parse(dataRetirada[2]);
+
+            int diaDevolucao = int.Parse(dataDevolucao[0]);
+            int mesDevolucao = int.Parse(dataDevolucao[1]);
+            int anoDevolucao = int.Parse(dataDevolucao[2]);
+
+            if ( anoDevolucao > 3000 || anoRetirada > 3000 || mesDevolucao > 12 || mesRetirada > 12 || diaDevolucao > 31 || diaRetirada > 31 )
+            {
+                Console.WriteLine("Err");
+            }
+            else if ( anoDevolucao > anoRetirada )
+            {
+                multa = 10000;
+            }
+            else if ( anoDevolucao == anoRetirada && mesDevolucao > mesRetirada )
+            {
+                multa = 500 * ( mesDevolucao - mesRetirada );
+            }
+            else if ( anoDevolucao == anoRetirada && mesDevolucao == mesRetirada && diaDevolucao > diaRetirada )
+            {
+                multa = 15 * ( diaDevolucao - diaRetirada );
+            }
+
+            Console.WriteLine(multa);
         }
     }
 }
