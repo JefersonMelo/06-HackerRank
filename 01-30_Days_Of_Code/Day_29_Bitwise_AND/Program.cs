0b01bd8edd33ee2bfb4db4b593b4c84ae7754f98
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-04 16:12:08 -0300

    commit inicial

---
+++ 01-30_Days_Of_Code/Day_29_Bitwise_AND/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_29_Bitwise_AND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
