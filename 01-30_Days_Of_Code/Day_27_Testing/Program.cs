dd39d252dcec66544a10d261f013b789caef86ee
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-05 10:36:41 -0300

    commit final

--- 01-30_Days_Of_Code/Day_27_Testing/Program.cs
+++ 01-30_Days_Of_Code/Day_27_Testing/Program.cs
@@ -4,6 +4,8 @@ namespace Day_27_Testing
 {
     class Program
     {
+        //https://www.geeksforgeeks.org/minimum-number-of-jumps-to-reach-end-of-a-given-array/
+        //https://zavaprogramer.blogspot.com/2019/08/hacker-rank-day-27-testing-java-program.html
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ 02-BTG-Pactual/NumeroPrimo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'isPrime' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER number as parameter.
+     */
+
+    public static string isPrime(int number)
+    {
+        int cont = 0;
+        for ( int i = 2; i < number; i++ )
+        {
+            if ( number % i == 0 ) cont++;
+        }
+        if ( cont == 0 )
+        {
+            return "TRUE";
+        }
+        else
+        {
+            return "FALSE";
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int number = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.isPrime(number);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
